c8435c8faafd8ee6a2774802a64acee5d0c8ec26
Kim Minsu <kms48491000@g.skku.edu>
2024-09-23 00:00:16 +0900

    2024/09/22

--- src/gui/load_chat_window.py
+++ src/gui/load_chat_window.py
@@ -2,7 +2,7 @@ import json
 
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
-from PyQt5.QtCore import Qt, QThread
+from PyQt5.QtCore import Qt, QThread, pyqtSignal
 from PyQt5 import QtWidgets
 
 import autogen
@@ -11,6 +11,9 @@ from autogen import ConversableAgent, GroupChat, GroupChatManager
 from llms import make_chat
 
 class ChatWorker(QThread):
+    message_signal = pyqtSignal(object, list, object, object)
+    summary_signal = pyqtSignal(str)
+
     def __init__(self, chat_objects, init_message):
         super().__init__()
         self.chat_objects = chat_objects
@@ -25,6 +28,10 @@ class ChatWorker(QThread):
             message=self.init_message,
             summary_method=self.chat_objects['summary_method'],
         )
+        self.summary_signal.emit(chat_result.summary)
+    
+    def message_signal_handler(self, recipient, messages, sender, config):
+        self.message_signal.emit(recipient, messages, sender, config)
 
 form_create_edit_chat_class = uic.loadUiType("gui/ui/load_chat.ui")[0]
 class LoadChat(QDialog, form_create_edit_chat_class):
@@ -33,6 +40,7 @@ class LoadChat(QDialog, form_create_edit_chat_class):
         self.setupUi(self)
 
         self.btn_select_config.clicked.connect(self._btn_select_config)
+        self.btn_reset.clicked.connect(self._btn_reset)
         self.btn_stop.clicked.connect(self._btn_stop)
         self.btn_enter.clicked.connect(self._btn_enter)
 
@@ -45,12 +53,12 @@ class LoadChat(QDialog, form_create_edit_chat_class):
         self.chat_worker = None
 
     def _btn_select_config(self):
+        self.tb_chat.clear()
+        self.pte_user_input.clear()
+
         fname = QFileDialog.getOpenFileName(self)
         self.label_config_path.setText(fname[0])
         
-        # TODO: 파일을 읽어서 chating을 생성해야 함
-        # + init_message가 존재하면 해당 값으로 설정할 것
-
         print(fname[0])
         with open(fname[0], 'r') as f:
             self.chat_config = json.load(f)
@@ -58,6 +66,13 @@ class LoadChat(QDialog, form_create_edit_chat_class):
 
         self.chat_objects = make_chat(self.chat_config, self.message_callback)
         self.pte_user_input.setPlainText(self.chat_objects['init_message'])
+
+    def _btn_reset(self):
+        self.tb_chat.clear()
+        if self.chat_objects:
+            self.pte_user_input.setPlainText(self.chat_objects['init_message'])
+        self.chat_start_flag = False
+        self.chat_worker = None
 
     def _btn_stop(self):
         if self.chat_worker:
@@ -82,15 +97,36 @@ class LoadChat(QDialog, form_create_edit_chat_class):
         if self.chat_start_flag == False:
             self.chat_start_flag = True
             self.chat_worker = ChatWorker(self.chat_objects, init_message)
+            self.chat_worker.message_signal.connect(self.message_append_handler)
+            self.chat_worker.summary_signal.connect(self.summary_append_handler)
             self.chat_worker.start()
         else:
             print("btn enter pressed.")
     
     def message_callback(self, recipient, messages, sender, config):
+        """
+        This function is called when a message is sent from one agent to another
+        """
+        self.chat_worker.message_signal_handler(recipient, messages, sender, config)
+        return False, None
+    
+    def message_append_handler(self, recipient, messages, sender, config):
+        """
+        Main thread function to append messages to the chat window.
+        Called from ChatWorker.message_signal_handler
+        """
         sender_name = messages[-1]['name']
         new_text = messages[-1]['content']
         self.tb_chat.append(
-            f'\n          -------------------------------------------[{sender_name}]-------------------------------------------'
+            f'          -------------------------------------------[{sender_name}]-------------------------------------------\n'
             + new_text
         )
-        return False, None
+    
+    def summary_append_handler(self, summary):
+        """
+        Main thread function to append summary to the chat window.
+        """
+        self.tb_chat.append(
+            f'          -------------------------------------------[Summary]-------------------------------------------\n'
+            + summary
+        )
